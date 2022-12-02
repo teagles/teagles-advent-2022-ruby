b65cc08ad98210f167c6865891ce7e6a2ba222a3
Tim Eagles <tim.eagles.au@gmail.com>
2022-12-01 19:16:09 -0800

    2022-01-1: Complete.

--- challenges/2022/01/solution.rb
+++ challenges/2022/01/solution.rb
@@ -1,12 +1,19 @@
 # frozen_string_literal: true
 
+require 'pry'
+
 module Year2022
   class Day01 < Solution
     # @input is available if you need the raw data input
     # Call `data` to access either an array of the parsed data, or a single record for a 1-line input file
 
     def part_1
-      nil
+      highest = -Float::INFINITY
+      data.each do |elf|
+        calories = elf.sum
+        highest = calories if calories > highest
+      end
+      highest
     end
 
     def part_2
@@ -14,13 +21,23 @@ module Year2022
     end
 
     # Processes each line of the input file and stores the result in the dataset
-    # def process_input(line)
-    #   line.map(&:to_i)
-    # end
+    def process_input(line)
+      line.to_i
+    end
 
     # Processes the dataset as a whole
-    # def process_dataset(set)
-    #   set
-    # end
+    def process_dataset(set)
+      results = []
+      current_elf = []
+      set.each do |food_item|
+        if food_item.zero?
+          results << current_elf
+          current_elf = []
+        else
+          current_elf << food_item
+        end
+      end
+      results << current_elf unless current_elf.none?
+    end
   end
 end
--- spec/2022/01/solution_spec.rb
+++ spec/2022/01/solution_spec.rb
@@ -28,14 +28,14 @@ RSpec.describe Year2022::Day01 do
       expect(described_class.part_1(example_input)).to eq(24_000)
     end
 
-    it 'returns nil for my input' do
-      expect(described_class.part_1(input)).to eq(nil)
+    it 'returns 69206 for my input' do
+      expect(described_class.part_1(input)).to eq(69_206)
     end
   end
 
   describe 'part 2' do
-    it 'returns nil for the example input' do
-      expect(described_class.part_2(example_input)).to eq(nil)
+    it 'returns 45000 for the example input' do
+      expect(described_class.part_2(example_input)).to eq(45_000)
     end
 
     it 'returns nil for my input' do
