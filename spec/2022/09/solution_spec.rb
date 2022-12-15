cef4123d5561f928d5cdc557c6c86aadf1dedf4d
Tim Eagles <tim.eagles.au@gmail.com>
2022-12-14 19:16:53 -0800

    2022-09-1: Starting state.

---
+++ spec/2022/09/solution_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Year2022::Day09 do
+  let(:input) { File.read(File.join(File.dirname(__FILE__), '../../../challenges/2022/09/input.txt')) }
+  let(:example_input) do
+    <<~EXAMPLE_INPUT
+      R 4
+      U 4
+      L 3
+      D 1
+      R 4
+      D 1
+      L 5
+      R 2
+    EXAMPLE_INPUT
+  end
+
+  describe 'part 1' do
+    it 'returns 13 for the example input' do
+      expect(described_class.part_1(example_input)).to eq(13)
+    end
+
+    it 'returns nil for my input' do
+      expect(described_class.part_1(input)).to eq(nil)
+    end
+  end
+
+  describe 'part 2' do
+    it 'returns nil for the example input' do
+      expect(described_class.part_2(example_input)).to eq(nil)
+    end
+
+    it 'returns nil for my input' do
+      expect(described_class.part_2(input)).to eq(nil)
+    end
+  end
+end
