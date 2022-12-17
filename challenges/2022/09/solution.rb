027dc5a6216c7d3fed4bcd9a2916034c952aa0cc
Tim Eagles <tim.eagles.au@gmail.com>
2022-12-16 21:17:12 -0800

    2022-09-1 Complete.

--- challenges/2022/09/solution.rb
+++ challenges/2022/09/solution.rb
@@ -1,12 +1,59 @@
 # frozen_string_literal: true
 
+require 'pry-byebug'
+
 module Year2022
   class Day09 < Solution
     # @input is available if you need the raw data input
     # Call `data` to access either an array of the parsed data, or a single record for a 1-line input file
+    INPUT_MATCHER = /([LRUD]) (\d+)/
+    Move = Struct.new(:direction, :distance)
+    Point = Struct.new(:x, :y)
+    RopePair = Struct.new(:head, :tail, :visited)
+    DIRECTIONS = { U: Point.new(0, 1).freeze,
+                   D: Point.new(0, -1).freeze,
+                   L: Point.new(-1, 0).freeze,
+                   R: Point.new(1, 0).freeze }.freeze
 
     def part_1
-      nil
+      rope = RopePair.new(Point.new(0, 0), Point.new(0, 0), Set[Point.new(0, 0)])
+      data.each do |move|
+        apply_move(rope, move)
+      end
+      rope.visited.length
+    end
+
+    def apply_move(pair, move)
+      Range.new(1, move.distance).each do |_step|
+        move_pair(pair, move.direction)
+      end
+    end
+
+    def move_pair(pair, direction)
+      pair.head = move_point(pair.head, direction)
+      pair.tail = move_tail(pair)
+      pair.visited << pair.tail
+    end
+
+    def move_tail(pair)
+      diff = diff_point(pair.head, pair.tail)
+      if diff.x.abs <= 1 && diff.y.abs <= 1
+        pair.tail
+      elsif diff.x.zero?
+        move_point(pair.tail, Point.new(0, diff.y / diff.y.abs))
+      elsif diff.y.zero?
+        move_point(pair.tail, Point.new(diff.x / diff.x.abs, 0))
+      else
+        move_point(pair.tail, Point.new(diff.x / diff.x.abs, diff.y / diff.y.abs))
+      end
+    end
+
+    def diff_point(point_a, point_b)
+      Point.new(point_a.x - point_b.x, point_a.y - point_b.y)
+    end
+
+    def move_point(point, direction)
+      Point.new(point.x + direction.x, point.y + direction.y)
     end
 
     def part_2
@@ -14,9 +61,10 @@ module Year2022
     end
 
     # Processes each line of the input file and stores the result in the dataset
-    # def process_input(line)
-    #   line.map(&:to_i)
-    # end
+    def process_input(line)
+      parsed = line.match(INPUT_MATCHER)
+      Move.new(DIRECTIONS[parsed[1].to_sym], parsed[2].to_i)
+    end
 
     # Processes the dataset as a whole
     # def process_dataset(set)
--- spec/2022/09/solution_spec.rb
+++ spec/2022/09/solution_spec.rb
@@ -17,19 +17,36 @@ RSpec.describe Year2022::Day09 do
     EXAMPLE_INPUT
   end
 
+  let(:larger_example_input) do
+    <<~EXAMPLE_INPUT
+      R 5
+      U 8
+      L 8
+      D 3
+      R 17
+      D 10
+      L 25
+      U 20
+    EXAMPLE_INPUT
+  end
+
   describe 'part 1' do
     it 'returns 13 for the example input' do
       expect(described_class.part_1(example_input)).to eq(13)
     end
 
-    it 'returns nil for my input' do
-      expect(described_class.part_1(input)).to eq(nil)
+    it 'returns 5858 for my input' do
+      expect(described_class.part_1(input)).to eq(5858)
     end
   end
 
   describe 'part 2' do
-    it 'returns nil for the example input' do
-      expect(described_class.part_2(example_input)).to eq(nil)
+    it 'returns 1 for the example input' do
+      expect(described_class.part_2(example_input)).to eq(1)
+    end
+
+    it 'returns 9 for the larger example input' do
+      expect(described_class.part_2(larger_example_input)).to eq(9)
     end
 
     it 'returns nil for my input' do
