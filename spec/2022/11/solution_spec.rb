3b039685db4d48563550381266bbb1f959fa2c50
Tim Eagles <tim.eagles.au@gmail.com>
2022-12-17 16:09:34 -0800

    2022-11-1: Starting state.

---
+++ spec/2022/11/solution_spec.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Year2022::Day11 do
+  let(:input) { File.read(File.join(File.dirname(__FILE__), '../../../challenges/2022/11/input.txt')) }
+  let(:example_input) do
+    <<~EXAMPLE_INPUT
+      Monkey 0:
+        Starting items: 79, 98
+        Operation: new = old * 19
+        Test: divisible by 23
+          If true: throw to monkey 2
+          If false: throw to monkey 3
+
+      Monkey 1:
+        Starting items: 54, 65, 75, 74
+        Operation: new = old + 6
+        Test: divisible by 19
+          If true: throw to monkey 2
+          If false: throw to monkey 0
+
+      Monkey 2:
+        Starting items: 79, 60, 97
+        Operation: new = old * old
+        Test: divisible by 13
+          If true: throw to monkey 1
+          If false: throw to monkey 3
+
+      Monkey 3:
+        Starting items: 74
+        Operation: new = old + 3
+        Test: divisible by 17
+          If true: throw to monkey 0
+          If false: throw to monkey 1
+    EXAMPLE_INPUT
+  end
+
+  describe 'part 1' do
+    it 'returns 10605 for the example input' do
+      expect(described_class.part_1(example_input)).to eq(10_605)
+    end
+
+    it 'returns nil for my input' do
+      expect(described_class.part_1(input)).to eq(nil)
+    end
+  end
+
+  describe 'part 2' do
+    it 'returns nil for the example input' do
+      expect(described_class.part_2(example_input)).to eq(nil)
+    end
+
+    it 'returns nil for my input' do
+      expect(described_class.part_2(input)).to eq(nil)
+    end
+  end
+end
